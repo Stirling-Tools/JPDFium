3244b396db826dafbbdbdce7d429281deb55ac96
Balázs Szücs <bszucs1209@gmail.com>
2026-09-25 17:26:27 +0200

    docs(bindings): describe the optional FreeText font symbol

--- jpdfium/src/main/java/stirling/software/jpdfium/panama/EmbedPdfAnnotationBindings.java
+++ jpdfium/src/main/java/stirling/software/jpdfium/panama/EmbedPdfAnnotationBindings.java
@@ -178,7 +178,7 @@ public final class EmbedPdfAnnotationBindings {
     public static final MethodHandle EPDFAnnot_GetVerticalAlignment = downcallOptional("EPDFAnnot_GetVerticalAlignment",
             FunctionDescriptor.of(JAVA_INT, ADDRESS));
 
-    /** Set FreeText appearance from a registered font id. Optional: new API. */
+    /** Set FreeText appearance from a registered font id. Optional: absent from some builds. */
     public static final MethodHandle EPDFAnnot_SetDefaultAppearanceRegisteredFont = downcallOptional("EPDFAnnot_SetDefaultAppearanceRegisteredFont",
             FunctionDescriptor.of(JAVA_INT, ADDRESS, JAVA_INT, JAVA_FLOAT, JAVA_INT, JAVA_INT, JAVA_INT));
 
